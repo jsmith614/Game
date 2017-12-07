1b4f1ec2812881b60ef644e4c50a8fd0ae82d82f
JSmith <jared.cpp@gmail.com>
2017-12-06 21:57:04 -0500

    image changes

--- Game2017/Constants/Constants.cs
+++ Game2017/Constants/Constants.cs
@@ -6,7 +6,7 @@ namespace Game2017
         public const string Penguins = "Penguins";
         public const string Map = "Map";
         public const string Map1 = "Level1";
-        public const string Player_Idle = "Player_Idle";
+        public const string Player_Idle = "Player_Idle2";
         public const string Player_Run = "Player_Run";
         public const string Player_Jump = "Player_Jump";
     }
